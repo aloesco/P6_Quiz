f1b7c6263991d1ce606858b61c3d681102444fdf
aloesco <aesco96@gmail.com>
2018-06-11 12:06:46 +0200

    v2

--- routes/index.js
+++ routes/index.js
@@ -120,8 +120,7 @@ router.delete('/quizzes/:quizId(\\d+)',
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
-router.get('/quizzes/randomplay', quizController.randomPlay);
-router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomCheck);
+
 
 router.post('/quizzes/:quizId(\\d+)/tips',
     sessionController.loginRequired,
